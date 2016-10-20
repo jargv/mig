8e955359732fe240ac13de6544c58b39663aa9aa
Jonathan Geddes <geddes.jonathan@gmail.com>
2016-10-19 23:09:22 -0600

    Add a bunch of tests and a Prereq feature

--- mig.go
+++ mig.go
@@ -1,79 +1,110 @@
-package migration
+package mig
 
 import (
 	"crypto/md5"
 	"encoding/base64"
+	"fmt"
 
 	"github.com/jmoiron/sqlx"
 )
 
-var registeredMigrationSets []MigrationSet
+var registeredMigrationSets [][]Step
 
+// Step represents a single step in a migration
 type Step struct {
 	Migrate string
 	Revert  string
+	Prereq  string
 }
 
-type MigrationSet []Step
-
-func Register(set MigrationSet) {
+// Register queues a MigrationSet to be exectued when mig.Run(...) is called
+func Register(set []Step) {
 	registeredMigrationSets = append(registeredMigrationSets, set)
 }
 
+func clearMigrationsForNextTest() {
+	registeredMigrationSets = nil
+}
+
+// Run executes the migration Steps which have been registered by `mig.Register`
+// on the given database connection
 func Run(db *sqlx.DB) error {
-	for _, set := range registeredMigrationSets {
-		err := runSet(db, set)
-		if err != nil {
-			return err
+	err := checkMigrationTable(db)
+	if err != nil {
+		return err
+	}
+
+	sets := make([][]Step, len(registeredMigrationSets))
+	copy(sets, registeredMigrationSets)
+
+	recorded, err := collectRecordedMigrations(db)
+	if err != nil {
+		return err
+	}
+
+	for i, set := range sets {
+		sets[i] = rewindSet(set, recorded)
+	}
+
+	err = doReverts(db, recorded)
+	if err != nil {
+		return err
+	}
+
+	//todo: detect the case where there's no progress
+
+	mostRecentProgress := -1
+
+	for {
+		retry := false
+		for i, set := range sets {
+			newSet, progressMade, err := progressSet(db, set)
+			if err != nil {
+				return err
+			}
+
+			if progressMade {
+				mostRecentProgress = i
+			} else if mostRecentProgress == i && len(newSet) > 0 {
+				return fmt.Errorf("Unable to make progress on migration steps: %v", sets)
+			}
+
+			//if this set isn't done, we'll have to cycle back around
+			if len(newSet) > 0 {
+				retry = true
+			}
+
+			sets[i] = newSet
+		}
+
+		if !retry {
+			break
 		}
 	}
 
 	return nil
 }
 
-func runSet(db *sqlx.DB, steps []Step) error {
-	err := checkMigrationTable(db)
-	if err != nil {
-		return err
-	}
-
-	rows, err := db.Query(`
-	SELECT hash, down
-	FROM   migration
-	`)
-	if err != nil {
-		return err
-	}
-
-	recordedMigrations := map[string]string{}
-	for rows.Next() {
-		var hash, down string
-		err := rows.Scan(&hash, &down)
-		if err != nil {
-			break
-		}
-		recordedMigrations[hash] = down
-	}
-
-	if err := rows.Err(); err != nil {
-		return err
-	}
-
+func rewindSet(steps []Step, recorded map[string]string) []Step {
 	for len(steps) > 0 {
 		hash := createHash(steps[0].Migrate)
-		if _, ok := recordedMigrations[hash]; !ok {
+		if _, ok := recorded[hash]; !ok {
 			break //we've found the migration to rewind to!
 		}
-		delete(recordedMigrations, hash)
+		delete(recorded, hash)
 		steps = steps[1:]
 	}
 
+	return steps
+}
+
+func doReverts(db *sqlx.DB, reverts map[string]string) error {
 	// do the rewind process
 	// TODO: do this in reverse chronology!
-	for hash, down := range recordedMigrations {
+	for hash, down := range reverts {
 		_, err := db.Exec(`
-		DELETE from migration
-		WHERE  hash = $1
+			DELETE from migration
+			WHERE  hash = $1
 		`, hash)
 		if err != nil {
 			return err
@@ -85,40 +116,58 @@ func runSet(db *sqlx.DB, steps []Step) error {
 		}
 	}
 
-	//execute the remaining steps
-	for _, migration := range steps {
+	return nil
+}
+
+func progressSet(db *sqlx.DB, steps []Step) ([]Step, bool, error) {
+	progress := false
+
+	for len(steps) > 0 {
+		step := steps[0]
+
+		if len(step.Prereq) > 0 {
+			_, err := db.Exec(step.Prereq)
+			if err != nil {
+				return steps, progress, nil
+			}
+		}
+
+		steps = steps[1:]
+
 		tx, err := db.Begin()
 
 		if err != nil {
-			return err
+			return nil, false, err
 		}
 
-		_, err = tx.Exec(migration.Migrate)
+		_, err = tx.Exec(step.Migrate)
 
 		if err != nil {
 			tx.Rollback()
-			return err
+			return nil, false, err
 		}
 
-		hash := createHash(migration.Migrate)
+		hash := createHash(step.Migrate)
 
 		_, err = tx.Exec(`
-		INSERT into migration (hash, down)
-		VALUES ($1, $2);
-		`, hash, migration.Revert)
+			INSERT into migration (hash, down)
+			VALUES ($1, $2);
+		`, hash, step.Revert)
 
 		if err != nil {
 			tx.Rollback()
-			return err
+			return nil, false, err
 		}
 
 		err = tx.Commit()
 		if err != nil {
-			return err
+			return nil, false, err
 		}
+
+		progress = true
 	}
 
-	return nil
+	return steps, progress, nil
 }
 
 func createHash(str string) string {
@@ -136,11 +185,35 @@ func checkMigrationTable(db *sqlx.DB) error {
 
 	//TODO: timestamps and other audit trails
 	_, err = db.Exec(`
-	CREATE TABLE migration (
-		hash TEXT,
-		down TEXT
-	)
+		CREATE TABLE migration (
+			hash TEXT,
+			down TEXT
+		)
 	`)
 
 	return err
+}
+
+func collectRecordedMigrations(db *sqlx.DB) (map[string]string, error) {
+	//collect the recored migrations
+	rows, err := db.Query(`
+	SELECT hash, down
+	FROM   migration
+	`)
+	if err != nil {
+		return nil, err
+	}
+
+	recorded := map[string]string{}
+	for rows.Next() {
+		var hash, down string
+		rows.Scan(&hash, &down)
+		recorded[hash] = down
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return recorded, nil
 }
---
+++ mig_test.go
@@ -0,0 +1,158 @@
+package mig
+
+import (
+	"log"
+	"os/exec"
+	"testing"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+func getConnection(t *testing.T) *sqlx.DB {
+	exec.Command("dropdb", "testPostgres").Run()
+	output, err := exec.Command("createdb", "testPostgres").CombinedOutput()
+	if err != nil {
+		log.Printf("couldn't create postgres db: %v, %s\n", err, output)
+	}
+
+	db, err := sqlx.Connect("postgres", "postgres://testuser:testpassword@localhost/testPostgres")
+	if err != nil {
+		t.Fatalf("couldn't connect to postgres test db: %v\n", err)
+	}
+
+	return db
+}
+
+func TestRevert(t *testing.T) {
+	db := getConnection(t)
+	Register([]Step{
+		{
+			Migrate: `create table survive(val int)`,
+		},
+		{
+			Migrate: `
+			  create table test_user (
+					name TEXT,
+					food TEXT
+				)
+			`,
+			Revert: `drop table test_user`,
+		},
+	})
+
+	err := Run(db)
+	if err != nil {
+		t.Fatalf(": %v\n", err)
+	}
+
+	stmt := `insert into survive (val) values (42)`
+	if _, err = db.Exec(stmt); err != nil {
+		t.Fatalf("couldn't insert: %v\n", err)
+	}
+
+	stmt = `insert into test_user (name, food) values ('Jonathan', 'crab'), ('Sarah', 'ice cream')`
+	if _, err = db.Exec(stmt); err != nil {
+		t.Fatalf("couldn't insert: %v\n", err)
+	}
+
+	var result1 []struct {
+		Name string `db:"name"`
+		Food string `db:"food"`
+	}
+
+	db.Select(&result1, "select * from test_user")
+	if len(result1) != 2 {
+		t.Fatalf(`len(result) != 2, len(result) == "%v"`, len(result1))
+	}
+
+	clearMigrationsForNextTest()
+
+	Register([]Step{
+		{
+			Migrate: `create table survive(val int)`,
+		},
+		{
+			Migrate: `
+				create table test_user (
+					name TEXT,
+					tv   TEXT
+				)
+			`,
+		},
+	})
+	err = Run(db)
+	if err != nil {
+		t.Fatalf(": %v\n", err)
+	}
+
+	stmt = `insert into test_user (name, tv) values ('Jonathan', 'Rick and Morty'), ('Sarah', 'The Office')`
+	if _, err = db.Exec(stmt); err != nil {
+		t.Fatalf("couldn't insert: %v\n", err)
+	}
+
+	var result2 []struct {
+		Name string `db:"name"`
+		Tv   string `db:"tv"`
+	}
+	db.Select(&result2, "select * from test_user")
+	if len(result2) != 2 {
+		t.Fatalf(`len(result2) != 2, len(result2) == "%v"`, len(result2))
+	}
+
+	var surviver struct {
+		Val int
+	}
+	err = db.Get(&surviver, `select val from survive limit 1`)
+	if err != nil {
+		t.Fatalf("table 'survive' didn't survive as expected: %v\n", err)
+	}
+	if surviver.Val != 42 {
+		t.Fatalf(`surviver.Val != 42, surviver.Val == "%v"`, surviver.Val)
+	}
+}
+
+func TestPrereq(t *testing.T) {
+	db := getConnection(t)
+
+	Register([]Step{
+		{
+			Prereq: `
+			  select 1 from test_prereq
+			`,
+			Migrate: `
+			  alter table test_prereq add column food text
+			`,
+		},
+	})
+
+	Register([]Step{
+		{
+			Migrate: `
+			  create table test_prereq()
+			`,
+		},
+	})
+
+	err := Run(db)
+	if err != nil {
+		t.Fatalf("couldn't run migrations: %v\n", err)
+	}
+
+	_, err = db.Exec("insert into test_prereq(food) values ('nachos'), ('burritos')")
+	if err != nil {
+		t.Fatalf("couldn't run migration: %v\n", err)
+	}
+
+	var result []struct {
+		Food string
+	}
+	err = db.Select(&result, "select * from test_prereq")
+	if err != nil {
+		t.Fatalf(": %v\n", err)
+	}
+
+	if len(result) != 2 {
+		t.Fatalf(`len(result) != 2, len(result) == "%v"`, len(result))
+	}
+}
