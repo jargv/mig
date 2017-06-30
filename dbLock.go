872bf31f376940c21de161d5ea263f4659f0db46
Jonathan Geddes <geddes.jonathan@gmail.com>
2017-06-30 15:34:49 -0600

    Make the dbLock code more robust and less prone to deadlock

--- dbLock.go
+++ dbLock.go
@@ -3,8 +3,11 @@ package mig
 import (
 	"errors"
 	"log"
+	"math/rand"
 	"sync"
 	"time"
+
+	"github.com/go-sql-driver/mysql"
 )
 
 var mutex = sync.Mutex{}
@@ -14,34 +17,57 @@ func WithDatabaseLock(db DB, timeout time.Duration, callback func() error) error
 	start := time.Now()
 
 	_, _ = db.Exec(`
-		CREATE TABLE MIG_DATABASE_LOCK (
-			lock_row int,
+		CREATE TABLE MIG_DATABASE_LOCK_V2 (
+			id       BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			lock_row INT,
 			UNIQUE (lock_row)
 		)
 	`)
 
+	var lockId int64
 	for {
-		_, err := db.Exec(`
-			INSERT INTO MIG_DATABASE_LOCK (lock_row)
+		res, err := db.Exec(`
+			INSERT INTO MIG_DATABASE_LOCK_V2 (lock_row)
 			VALUES      (1)
 		`)
 		if err == nil {
-			break
+			lockId, err = res.LastInsertId()
+			if err != nil {
+				log.Printf("error trying to get LastInsertId: %s", err)
+			} else {
+				break
+			}
 		}
 
-		log.Printf("err: %#v", err)
+		if mysqlErr, ok := err.(*mysql.MySQLError); ok && mysqlErr.Number == 0x426 {
+			// this is the only error we do nothing with
+		} else {
+			log.Printf("err: %#v", err)
+		}
 
 		if time.Now().Sub(start) > timeout {
 			return ErrDatabaseLockTimout
 		}
 
-		time.Sleep(1500 * time.Millisecond)
+		// variable backoff between 0.5 and 1.5 seconds
+		sleepTime := time.Duration((0.5 + rand.Float32()) * float32(time.Second))
+		time.Sleep(sleepTime)
 	}
 
 	defer func() {
-		_, _ = db.Exec(`
-			DELETE FROM MIG_DATABASE_LOCK
-		`)
+		for {
+			_, err := db.Exec(`
+				DELETE FROM MIG_DATABASE_LOCK_V2
+				WHERE id = ?
+			`, lockId)
+
+			if err == nil {
+				break
+			}
+
+			log.Printf("error releasing lock: %v", err)
+			time.Sleep(100 * time.Millisecond)
+		}
 	}()
 
 	return callback()
